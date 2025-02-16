0c0d2cb098b00138de575e54411ecc73fd317822
huuhoai456 <dshzlk@gmail.com>
2025-02-16 10:53:41 +0700

    Cập nhật code
    thêm dòng

--- HelloWorld/src/tiil/edu/XinChao.java
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -14,6 +14,7 @@ public class XinChao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Tôi lần đầu học Java, vad đang thử nghiệm quản lý mã nguồn với GIT ");
+		System.out.print("Tôi thêm một dòng mới");
 	}
 
 }
